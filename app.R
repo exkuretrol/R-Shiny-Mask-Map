d7e5e4914ee91150bb4fee64e31e70f552edc9bf
exkuretrol <33695301+exkuretrol@users.noreply.github.com>
2021-06-13 03:58:25 +0800

    No, I didnt fix it :v.

--- app.R
+++ app.R
@@ -237,11 +237,6 @@ server <- function(input, output, session) {
                 label = "鄉/鎮/市/區:",
                 choices = x
             )
-            if (rv$update) {
-                rv$map_data <- Institute_data %>%
-                    filter(City == input$drawer1)
-                rv$update <- FALSE
-            }
         }    
     })
 
@@ -252,7 +247,6 @@ server <- function(input, output, session) {
             rv$map_data <- Institute_data %>%
                 filter(City == input$drawer1)
                 
-            rv$update <- TRUE
         }  else if (input$drawer1 != "請選擇縣市" & input$drawer2 != "請選擇鄉/鎮/市/區") {
 
              
@@ -271,7 +265,7 @@ server <- function(input, output, session) {
                 rv$map_data <- data
                 
             }
-
+            
         } else {
 
             rv$map_data <- Institute_data
